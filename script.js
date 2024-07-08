1d8a540c4ff446786f7d12bb43b64b73b48c7492
Abdulkadyr <damadevkadir10@gmail.com>
2024-07-09 00:34:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 let cart = [];
 
+// Обработчик событий для кнопок "Добавить в корзину"
 document.querySelectorAll('.add-to-cart').forEach(button => {
     button.addEventListener('click', () => {
         const name = button.getAttribute('data-name');
-        const price = button.getAttribute('data-price');
+        const price = parseInt(button.getAttribute('data-price'), 10); // Преобразование в число
         cart.push({ name, price });
         updateCart();
+        console.log(`Добавлен товар: ${name}, цена: ${price}`); // Отладочное сообщение
     });
 });
 
+// Обработчик событий для кнопки "Перейти к оплате"
 document.getElementById('checkout').addEventListener('click', () => {
     const cartData = JSON.stringify(cart);
-    // Отправка данных в телеграм-бота
-    fetch('/checkout', {
+    console.log("Корзина:", cartData); // Отладочное сообщение
+
+    // Отправка данных на сервер
+    fetch('https://f4d5-195-19-120-238.ngrok-free.app/checkout', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
         body: cartData
     })
-        .then(response => response.json())
+        .then(response => {
+            console.log("Ответ сервера:", response); // Отладочное сообщение
+            if (!response.ok) {
+                throw new Error(`Ошибка HTTP: ${response.status}`);
+            }
+            return response.json();
+        })
         .then(data => {
+            console.log("Данные от сервера:", data); // Отладочное сообщение
             if (data.success) {
+                console.log("Успешный ответ от сервера", data); // Отладочное сообщение
                 window.location.href = `https://t.me/${data.chat}`;
             } else {
                 alert('Ошибка при переходе к оплате');
