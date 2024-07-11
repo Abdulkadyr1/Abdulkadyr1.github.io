b1b71c05af8de51138a8f42b1a2cc8fd16fab71c
Abdulkadyr <damadevkadir10@gmail.com>
2024-07-11 16:07:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById('checkout').addEventListener('click', () => {
     const cartData = JSON.stringify({ cart: cart });
     console.log("Cart data to be sent:", cartData);
 
-    fetch('https://e3e7-185-244-20-32.ngrok-free.app/checkout', {
+    fetch('https://b888-185-244-20-32.ngrok-free.app/checkout', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
